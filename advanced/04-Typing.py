56f2eb9de55b6cb9637b3c0b467ee4f9391153ee
Patataman <dan.rodriguezlopez@protonmail.com>
2018-04-04 17:10:06 +0200

    typing

--- advanced/04-Typing.py
+++ advanced/04-Typing.py
@@ -4,11 +4,66 @@
 
     Bueno, realmente sigue siendo no tipado, pero ./shrug
     https://docs.python.org/3/library/typing.html
+
+    Realmente no "tipa" nada este módulo, si no que permite indicar
+    qué tipo de dato esperamos de forma que sea todo más legible y entendible.
+    Todo sigue quedando en el lado del desarrollador para asegurarse que
+    las cosas se reciben como deben
 '''
 
-#Función que recibe un string y devuelve un string
+# Función que recibe un string y devuelve un string
 def greeting(name: str) -> str:
-    return 'Hello ' + name
-
+    if type(name) != str:
+        return "No no no, mal, string bien."
+    return 'Hello '.format(name)
 
 print(greeting("Pepe"))
+print(greeting(12)) #Funciona, pero no debería ser un string
+
+from threading import Thread
+
+''' Función que recibe cosas y devuelve un entero.
+    Como veis se puede indicar objetos como argumento
+    y de igual forma como resultado
+'''
+def foo(cosa1: int, cosa2: Thread, cosa3) -> int:
+    return cosa1
+
+''' Podemos crear "alias" de objetos o tipos para ahorrarnos
+    texto y ser un poco más descriptivos
+'''
+
+from typing import List, Tuple
+
+class Fruta():
+    pass
+
+#Por convenio los alias empiezan en mayúscula
+Cesta = List[Fruta]
+Compra = Tuple[Cesta, Cesta]
+
+def compra(elem1: Cesta, elem2: Cesta) -> Compra:
+    return [elem1, elem2]
+
+cesta_falsa = [Fruta]*5
+print(compra(cesta_falsa, cesta_falsa))
+
+''' O incluso crear nuevos tipos! D:
+    MENUDA BRUJERÍA
+'''
+
+from typing import NewType
+
+#Al fin y al cabo esto va como una lambda
+Droga = NewType('Droga', str)
+
+coca: Droga = Droga("Cocaina")
+farlopa: Droga = Droga("Farlopa")
+
+def goToJail(pruebas: List[Droga]) -> str:
+    if len(pruebas) > 0:
+        return "A la cárcel!"
+    else:
+        return "Está limpio"
+
+print(goToJail([coca, farlopa]))
--- advanced/05-Logging.py
+++ advanced/05-Logging.py
@@ -1,6 +1,15 @@
 ''' Hasta ahora lo máximo que teníamos para seguir el curso
     de Python eran los prints que ibamos poniendo en el código,
-    pero como de verdad habría que hacerlo es mediante logs.
+    pero si fuesemos desarrolladores bien pagados haciendo programas
+    super amasings que te cagas energy en Python, pues deberíamos
+    usar logs.
 
     Para eso está el querido módulo "logging".
+
+    https://docs.python.org/3/library/logging.html?highlight=logging#module-logging
+
+    Para los que no sepan buscar, aquí el tutorial básico:
+        https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
 '''
+
+import logging
