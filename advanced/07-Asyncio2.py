8b180bcd6ba0274fbd46ef9c49f3e96c91dde91c
Daniel Rodríguez L <dan.rodriguezlopez@protonmail.com>
2019-03-28 17:20:28 +0100

    Update 07-Asyncio2.py

--- advanced/07-Asyncio2.py
+++ advanced/07-Asyncio2.py
@@ -8,10 +8,19 @@ async def coroutine(i):
     await asyncio.sleep(4)
 
     print('coroutine_{} resumed. coroutine_1 exiting'.format(i))
+    
+    # Podemos devolver cosas en los async
+    return i
 
 
 # this is the event loop
 loop = asyncio.get_event_loop()
 
 # schedule both the coroutines to run on the event loop
-loop.run_until_complete(asyncio.gather(*[coroutine(i) for i in range(5)]))
+res = loop.run_until_complete(asyncio.gather(*[coroutine(i) for i in range(5)]))
+
+# la variable "res" va a ser una lista con los resultados de las llamadas
+print(res)
+
+# buenas prácticas
+loop.close()
