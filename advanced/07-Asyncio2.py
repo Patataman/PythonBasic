6451479f3b553025cbda887f342ab4deeef2d735
Daniel Rodríguez L <dan.rodriguezlopez@protonmail.com>
2019-03-29 12:10:13 +0100

    with tqdm & requests

--- advanced/07-Asyncio2.py
+++ advanced/07-Asyncio2.py
@@ -1,26 +1,32 @@
+from tqdm import tqdm
+
 import asyncio
+import requests
 
 # definition of a coroutine
 async def coroutine(i):
     print('coroutine_{} is active on the event loop'.format(i))
 
     print('coroutine_{} yielding control. Going to be blocked for 4 seconds'.format(i))
-    await asyncio.sleep(4)
+    res = await loop.run_in_executor(
+        None,
+        requests.get,
+        'http://googles.es/'
+    )
 
     print('coroutine_{} resumed. coroutine_1 exiting'.format(i))
-    
-    # Podemos devolver cosas en los async
-    return i
+    return res
 
 
 # this is the event loop
 loop = asyncio.get_event_loop()
 
-# schedule both the coroutines to run on the event loop
-res = loop.run_until_complete(asyncio.gather(*[coroutine(i) for i in range(5)]))
+tasks2 = [coroutine(i) for i in range(5)]
 
-# la variable "res" va a ser una lista con los resultados de las llamadas
-print(res)
+import pdb; pdb.set_trace()
+res = []
+for i in tqdm(asyncio.as_completed(tasks2), total=5):
+    res.append(loop.run_until_complete(i))
+print("pepe", res)
 
-# buenas prácticas
 loop.close()
