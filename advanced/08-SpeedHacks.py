82ee32100fe358d236dab2f42f3501b2c2ad81bb
Daniel Rodríguez L <dan.rodriguezlopez@protonmail.com>
2022-05-20 10:35:19 +0200

    Create 08-SpeedHacks.py

---
+++ advanced/08-SpeedHacks.py
@@ -0,0 +1,5 @@
+""" Lista de hacks que se pueden hacer para que el código vaya más rápido.
+    Algunos son por implementación del lenguaje, otros por trucos que puede hacer el intérprete
+"""
+
+# TODO
